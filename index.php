51b0f0b10cedc70800df035f7d64e0f87d670cf6
vale075 <63117317+vale075@users.noreply.github.com>
2025-06-24 22:53:55 +0200

    Ajout d'un favicon

--- index.php
+++ index.php
@@ -20,6 +20,26 @@ $base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/\\') . '/'; // récupération 
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-15" />
 	<base href="<?= $base ?>">
 	<title>La REZerve</title>
+
+	<link rel="apple-touch-icon" sizes="57x57" href="ressources/icons/favicon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="ressources/icons/favicon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="ressources/icons/favicon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="ressources/icons/favicon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="ressources/icons/favicon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="ressources/icons/favicon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="ressources/icons/favicon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="ressources/icons/favicon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="ressources/icons/favicon-180x180.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="ressources/icons/favicon-16x16.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="ressources/icons/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="ressources/icons/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="ressources/icons/favicon-192x192.png">
+	<link rel="shortcut icon" type="image/x-icon" href="ressources/icons/favicon.ico">
+	<link rel="icon" type="image/x-icon" href="ressources/icons/favicon.ico">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="ressources/icons/favicon-144x144.png">
+	<meta name="msapplication-config" content="ressources/icons/browserconfig.xml">
+
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
