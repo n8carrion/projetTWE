b28c28b2570b9efbdfc1b9a9531e13b5766cd795
Nejea <98034812+Nejea@users.noreply.github.com>
2025-06-24 22:10:48 +0200

    Changement header et footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,91 +1,35 @@
-<style>
-#banniere {
-        background-color:rgb(240, 240, 240);
-        border : 1px solid black;
-        padding: 10px;
-        margin:5px;
-        position : relative;
-        height: 15%;
-    }
-#banniere div {
-    display:inline;
-}
-#logo img {
-    width: 200px;
-    position:absolute ;
-    left:2%;
-
-}
-#menu a {
-    margin:5px;
-    position:absolute;
-    top:25%;
-}
-
-#lienAcceuil {
-    left:20% ;
-}
-#lienCatalogue {
-    left:25% ;
-}
-#lienAPropos {
-    left:31% ;
-}
-
-#Odd img {
-    width: 10%;
-    height: 80%;
-    display:inline ;
-    position: absolute ;
-    bottom:10%;
-}
-#footext {
-    position:absolute ;
-    top:4%;
-    left:50%;
-    right:25% ;
-    text-align:justify ;
-    /*font-weight:bolder;*/
-}
-#odd11 {
-    right: 13%;
-    
-}
-#odd12 {
-    right: 1%;
-}
-</style>
 
 
-<body>
-    <div id="banniere">
+    <div id="footer" class="banniere">
 
-        <!-- <h1 id="titre"> La REZerve </h1> -->
-        <!-- le logo est cliquable et contient un lien vers l'acceuil -->
-        <div id="logo">
-            <a href="accueil"><img src="ressources/logoLaREZerve1.png" alt="Logo de la REZerve"></a>
+        <a href="accueil">
+            <img id="logo" src="ressources/logoLaREZerve1.png" />
+        </a>
+        
+        
+
+        <div class="menu" style="display : inline">
+            <a  href="accueil">Accueil</a>
+            <a  href="catalogue">Catalogue</a>
+            <a  href="apropos">A propos</a>
         </div>
-        <div id="menu">
-            <a id ="lienAcceuil" href="accueil">Accueil </a> 
-            <a id ="lienCatalogue" href="catalogue">Catalogue </a> 
-            <a id = "lienAPropos" href="apropos">A Propos</a> 
-        </div>
-        <div>
-            <p id=footext>
-            
+
+        <div id="Odd">
+            <p id=textOdd>
                 La REZerve répond aux objectifs de 
                 développement durable (ODD) suivants: <br>
                 11. Villes et Communautés durables <br>
                 12. Consommation et production responsable
             </p>
-        </div>
-        <div id="Odd">
-            <a target="_blank" href="https://pactemondial.org/17-objectifs-developpement-durable/odd-11-villes-et-communautes-durables/">
-                <img id="odd11" src="ressources/ODD11.png" alt="ODD 11: Villes et Communautés durables">
-            </a>
-            <a target="_blank" href="https://pactemondial.org/17-objectifs-developpement-durable/odd-12-consommation-et-production-responsable/#:~:text=L'ODD%2012%20encourage%20sp%C3%A9cifiquement,produits%20nuisibles%20pour%20l'environnement.">
-             <img id="odd12" src="ressources/ODD12.png" alt="ODD12: Consommation et production ">
-            </a>
+
+            <div id="imgOdd">
+                <a target="_blank" href="https://pactemondial.org/17-objectifs-developpement-durable/odd-11-villes-et-communautes-durables/">
+                    <img id="odd11" src="ressources/ODD11.png" alt="ODD 11: Villes et Communautés durables">
+                </a>
+                <a target="_blank" href="https://pactemondial.org/17-objectifs-developpement-durable/odd-12-consommation-et-production-responsable/#:~:text=L'ODD%2012%20encourage%20sp%C3%A9cifiquement,produits%20nuisibles%20pour%20l'environnement.">
+                    <img id="odd12" src="ressources/ODD12.png" alt="ODD12: Consommation et production ">
+                </a>
+            </div>
         </div>
     
 
@@ -97,4 +41,3 @@
 
 </div>
 
-</body>
--- templates/header.php
+++ templates/header.php
@@ -1,72 +1,88 @@
-
+<!-- Style pour le header et le footer -->
 <style>
-    #banniere {
-        background-color:rgb(240, 240, 240);
-        border : 1px solid black;
-        padding: 10px;
-        position : relative;
+
+    /* STYLE POUR LES BOUTONS de classe "btn" */
+    .btn {
+        display: inline-block;
+    padding: 8px 15px;
+    background-color:rgb(170, 202, 41);
+    color: white;
+    /* enleve le texte souligné pour un lien */
+    text-decoration: none; 
+    border-radius: 3px;
+    font-weight: bold;
     }
-    #logo img {
-        width: 250px;
-        display:inline ;
-    }
-    #titre {
-        font-size: 24px;
-        margin: 10px 0;
+    .btn:hover {
+        background-color: rgb(150, 180, 30); /* Couleur au survol */
     }
 
-    a {
-        margin: 5px;
-        text-decoration: none;
-        text-style : bold;
+    /* STYLE POUR LE MENU DE NAVIGATION de classe "menu" */
+    .menu  a {
+    text-decoration: none; 
+    padding: 8px 15px;
+    color: white;
+    background-color:rgb(21, 60, 9); 
+    font-weight: bold;
+    border-radius: 3px;
+    /* margin-right: 5px; */
+    
     }
-    #banniere div {
-        display:inline ;
+
+    .menu a:hover {
+    background-color:rgb(71, 121, 33);
+    color: white;
     }
-    #barreSearch {
-        position:absolute ;
-        right: 300px;
+
+
+    /* STYLE POUR LES BANNIERE (header et footer), de classe "banniere" */
+    .banniere {
+        background-color:rgba(251,249,246,255); /* Couleur de fond */
+        padding: 10px;
+        border : 1px solid black;
+    
     }
-    #lienConnexion {
-        position:absolute;
-        right:10px;
+
+    .banniere a{
+        text-decoration: none; 
     }
-    #BtnAdd{
-        display:none ;
-        right: 100px ;
-        position:absolute ;
+
+
+    #logo  {
+        width: 200px; 
     }
+
+
+
+
+    #Odd img {
+        width: 70px; /* Largeur des images ODD */
+        height: 70px; /* Hauteur des images ODD */
+    }
+
 
 </style>
 
 
-<body>
+<div id="header" class="banniere">
 
-   
-
-    <div id="banniere">
-
-        <!-- <h1 id="titre"> La REZerve </h1> -->
-        <!-- le logo est cliquable et contient un lien vers l'acceuil -->
-        <div id="logo">
-            <a href="accueil"><img src="ressources/logoLaREZerve1.png" alt="Logo de la REZerve"></a>
-        </div>
-        <div id="menu">
-            <a id ="lienAcceuil" href="accueil">Accueil</a>
-            <a id ="lienCatalogue" href="catalogue">Catalogue</a>
-        </div>
-
-        <input id=barreSearch type="search" placeholder="Recherche d'une annonce">
-        <input id=BtnAdd type="button" value="+Ajouter une annonce">
-        <a id ="lienConnexion" href="login">Se connecter</a>
     
+        <a href="accueil">
+            <img id="logo" src="ressources/logoLaREZerve1.png" />
+        </a>
+    <div class="menu" style="display : inline">
+        <a id="lienAcceuil" href="accueil">Accueil</a>
+        <a id="lienCatalogue" href="catalogue">Catalogue</a>
+    </div>
 
-        <?php
-        // Si l'utilisateur n'est pas connecte, on affiche un lien de connexion 
-        // if (!valider("connecte","SESSION"))
-        // 	echo "<a id=\"lienConnexion\" href=\"index.php?view=login\">Se connecter</a>";
-        ?>
 
+
+    
+        <input id=barreSearch type="search" placeholder="Recherche d'une annonce">
+        <a class="btn" href="editionObjet">+ Ajouter une annonce</a>
+
+        <!-- TODO Le lien se connecter ne doit apparaitre que si on es connecté -->
+        
+    <div class="menu" style="display : inline">
+        <a id="lienSeConnecter" href="login">Se connecter</a>
+    </div>
 </div>
-
-</body>
