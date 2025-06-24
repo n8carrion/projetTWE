e10593542a3967163b51347b2b4537662188e00e
Tiplup1 <tan.william03@gmail.com>
2025-06-24 22:14:44 +0200

    debut accueil.php

--- templates/accueil.php
+++ templates/accueil.php
@@ -9,6 +9,31 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 
 ?>
 
-<p>Test Accueil</p>
+<script>
+    //rajouter le chargment des annonces
+ </script>
+
+<div>
+<p>La REZerve
+Ici, on donne une seconde vie aux objets sur la résidence de Centrale ! <br>
+
+Vous avez un grille-pain qui traîne ? Besoin d’une casserole ou d’un vidéoprojecteur pour le week-end ?
+Sur la REZerve vous pouvez donner ou emprunter facilement des objets entre résident·e·s et assos de Centrale Lille Institut. <br>
+
+Moins d’achats neufs, plus de seconde main, plus de partage.
+En participant, vous contribuez concrètement à une consommation plus responsable en phase avec les Objectifs de Développement Durable. <br>
+
+Explorez le catalogue ou proposez vos objets dès maintenant !
+
+</p>
+</div>
+
+<div>
+    <p> Les dernières annonces</p>
+</div>
+
+
+
+
 
 
