6f473c074ce80fdd4ff2838a994148ff91edafc6
Tiplup1 <tan.william03@gmail.com>
2025-06-25 09:09:36 +0200

    fonction infosUtilisateur + style Accueil

--- templates/accueil.php
+++ templates/accueil.php
@@ -2,8 +2,10 @@
     //rajouter le chargment des annonces
 </script>
 
-<div>
-<p>La REZerve
+<div id=description>
+<h1>La REZerve</h1>
+<img src="ressources/logoLaREZerve1.png">
+<p>
 Ici, on donne une seconde vie aux objets sur la résidence de Centrale ! <br>
 
 Vous avez un grille-pain qui traîne ? Besoin d’une casserole ou d’un vidéoprojecteur pour le week-end ?
@@ -13,12 +15,23 @@ Moins d’achats neufs, plus de seconde main, plus de partage.
 En participant, vous contribuez concrètement à une consommation plus responsable en phase avec les Objectifs de Développement Durable. <br>
 
 Explorez le catalogue ou proposez vos objets dès maintenant !
-
 </p>
+
 </div>
 
-<div>
-    <p> Les dernières annonces</p>
+<div class=banniere>
+    <h2 class="center"> Les dernières annonces</h2>
+</div>
+    <!-- Chaque objet est représenté par une "carte" -->
+    <div class="carte-objet">
+        <!-- Si on clique sur  -->
+        <a href="annonce/1"> <!-- annonce/idObjet -->
+            <img src="uploads/imagesObjets/2_1.jpg" alt="Photo de l’objet">
+            <h2>Table basse test</h2>
+            <p><strong>Type :</strong>Don</p>
+            <p><strong>Catégorie :</strong> Électroménager</p>
+            <p><strong>Statut :</strong> Disponible</p>
+        </a>
 </div>
 
 
