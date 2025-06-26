f5cd1d99ac096462b61380005738f147151b4351
n8carrion <nataliacarrions@gmail.com>
2025-06-26 11:17:16 +0200

    edition button

--- templates/ficheObjet.php
+++ templates/ficheObjet.php
@@ -65,7 +65,7 @@
 
         <div class="admin-options">
             <button> <!-- not sure yet if the link will be js or done just with <a></a>-->
-                <a href="annonce/1/">
+                <a href="annonce/1/edit">
                     Modifier l'annonce </a>
             </button>
 
